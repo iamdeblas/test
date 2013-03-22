9d5ad409083a38b5e41612d18fa1519ec06dec1e
Anthony Bruno <abdeblasio@gmail.com>
2013-03-22 11:31:43 -0400

    v1

---
+++ test.js
@@ -0,0 +1,18 @@
+$(document).ready (function(){
+	
+$('#test').click(function(){
+	window.location.href = "http://www.google.com";
+	//$(this).hide();
+	});
+
+$('#btest').click(function(){
+	window.location.href = "http://www.google.com";
+	//$(this).hide();
+	});
+
+$('#testfield').click(function(){
+	window.location.href = "http://www.google.com";
+	//$(this).hide();
+	});
+
+});
